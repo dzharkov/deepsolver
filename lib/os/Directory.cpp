d79048e77f5719e14145347de92c2460982106f9
Michael Pozhidaev <msp@altlinux.ru>
2011-11-07 14:46:27 +0700

    Added Directory::Iterator class

--- lib/os/Directory.cpp
+++ lib/os/Directory.cpp
@@ -34,3 +34,36 @@ void Directory::ensureExists(const std::string& path)
 	  if (!isExist(s))
 	    TRY_SYS_CALL(mkdir(s.c_str(), 0777) == 0, "mkdir(" + s + ")");
 }
+
+bool Directory::Iterator::moveNext()
+{
+  struct dirent* ent = readdir(m_dir);;
+  if (ent)
+    {
+      m_currentName = ent->d_name;
+      return 1;
+    }
+  closedir(m_dir);
+  m_dir = NULL;
+  return 0;
+}
+
+std::string Directory::Iterator::getName() const
+{
+  assert(m_dir);//m_currentName has a valid value;
+  return m_currentName;
+}
+
+std::string Directory::Iterator::getFullPath() const
+{
+  assert(m_dir);//m_currentName has a valid value;
+  return concatUnixPath(m_path, m_currentName);
+}
+
+std::auto_ptr<Directory::Iterator> Directory::enumerate(const std::string& path)
+{
+  DIR* dir = opendir(path.c_str());
+  if (!dir)
+    SYS_STOP("opendir(" + path + ")");
+  return std::auto_ptr<Iterator>(new Iterator(path, dir));
+}
--- lib/os/Directory.h
+++ lib/os/Directory.h
@@ -5,8 +5,33 @@
 class Directory
 {
 public:
+  class Iterator
+  {
+  public:
+    Iterator(const std::string& path, DIR* dir)
+      : m_path(path), m_dir(dir) {}
+
+    virtual ~Iterator()
+    {
+      if (m_dir)
+	closedir(m_dir);
+    }
+
+  public:
+    bool moveNext();
+    std::string getName() const;
+    std::string getFullPath() const;
+
+  private:
+    std::string m_path;
+    DIR* m_dir;
+    std::string m_currentName;
+  }; //class Iterator;
+
+public:
   static bool isExist(const std::string& path);
   static void ensureExists(const std::string& path);
+  static std::auto_ptr<Iterator> enumerate(const std::string& path);
 }; //class Directory;
 
 #endif //FIXME_DIRECTORY_H;
