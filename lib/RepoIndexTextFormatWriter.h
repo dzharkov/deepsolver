3d6258d50e60379cee6581b2f1ae38319dc491ef
Michael Pozhidaev <msp@altlinux.ru>
2011-11-23 20:02:10 +0700

    Added Doxyfile and text description for RepoIndexTextFormatWriter class

--- lib/RepoIndexTextFormatWriter.h
+++ lib/RepoIndexTextFormatWriter.h
@@ -6,6 +6,38 @@
 #include"NamedPkgRel.h"
 #include"AbstractConsoleMessages.h"
 
+/**\brief The class to write repo index data in text format
+ *
+ * This is the main class for writing repository index data in text
+ * format. It must be used by IndexCore class only and not purposed for
+ * direct using by library client applications. By the way it performs
+ * provides filtering with the options used during the invocation and
+ * saves provide resolving data. To be memory-efficient this class
+ * creates and then removes one or more temporary files. It means that
+ * directory for repository index must have more free disk space than
+ * just for index data itself.
+ *
+ * The work consists of three steps: initialization, package data
+ * collecting and commit. On initialization only temporary file for
+ * package data storing is created . On every add operation during the
+ * collecting step all required information about single package file is
+ * written to this temporary file. Main work is done on commit step. This
+ * step makes provides filtering and building provide resolving table. 
+ *
+ * Provide resolving table shows what packages can be used for every
+ * provide entry dependency. Building this information during repository
+ * index creation can significantly reduce time used for package
+ * installation and removing operations. 
+ *
+ * There are two options for provide filtering:register the provide entry
+ * if corresponding require dependency is present and filter provides by
+ * directories list. The second case is actual only for provides as files
+ * from the file list of the package. The filtering mode by require
+ * dependencies includes dependencies from collected packages and in
+ * additional from optional string list.
+ * \sa IndexCore RepoIndexInfoFile
+ * 
+ */
 class RepoIndexTextFormatWriter
 {
 public:
