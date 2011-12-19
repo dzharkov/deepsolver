88050a7544c28b8428596dcf7c25bd6154ab3603
Michael Pozhidaev <msp@altlinux.ru>
2011-12-20 02:19:40 +0700

    MD5 regression test completed

--- tests/regression/md5/test.cpp
+++ tests/regression/md5/test.cpp
@@ -3,7 +3,7 @@
 #include"depsolver.h"
 #include"MD5.h"
 
-void writeFile(const std:;string& fileName, size_t len)
+void writeFile(const std::string& fileName, size_t len)
 {
   File f;
   f.create(fileName);
@@ -15,7 +15,8 @@ void writeFile(const std:;string& fileName, size_t len)
       const size_t count = len > written + sizeof(buf)?sizeof(buf):len - written;
       for(size_t i = 0;i < count;i++)
 	buf[i] = (unsigned char)(rand() % 256);
-      file.write(buf, count);
+      f.write(buf, count);
+      written += count;
     }
 }
 
@@ -26,9 +27,13 @@ int main(int argc, char* argv[])
   size_t len = 0;
   if (!(s >> len))
     {
-      std::cerr << "\'" << argv[2] << "\' is not a valid length"
+      std::cerr << "\'" << argv[2] << "\' is not a valid length" << std::endl;
 	return 1;
     }
   writeFile(argv[1], len);
+  MD5 md5;
+  md5.init();
+  md5.updateFromFile(argv[1]);
+  std::cout << md5.commit(argv[1]) << std::endl;
   return 0;
 }
