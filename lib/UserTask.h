65446ee438232b4589f459e94691ebe9b1882014
Michael Pozhidaev <msp@altlinux.ru>
2011-12-27 04:05:20 +0700

    New approach to UserTask class

--- lib/UserTask.h
+++ lib/UserTask.h
@@ -2,53 +2,31 @@
 #ifndef DEPSOLVER_USER_TASK_H
 #define DEPSOLVER_USER_TASK_H
 
-enum {
-  TaskItemNone = 0,
-  TaskItemInstall = 1,
-  TaskItemRemove = 2;
-};
-
-class UserTaskItem
+struct UserTaskItemToInstall
 {
-public:
-  UserTaskItem(),
-    packageId(BAD_PACKAGE_ID),
-    less(0),
-    equals(0),
-    greater(0) {}
-
-  UserTaskItem(int t, const std::string& name)
-    : type(t),
-      pkgName(name),
-      packageId(BAD_PACKAGE_ID)
-      less(0),
+  UserTaskItemToInstall()
+    : less(0),
       equals(0),
       greater(0) {}
 
-  UserTaskItem(int t,
-	       const std::string& name,
-	       const std::string& ver,
-	       bool l = 0,
-	       bool e = 1,
-	       bool g = 0)
-    : type(t),
-      pkgName(name),
-      packageId(BAD_PACKAGE_ID),
-      version(ver),
-      less(l),
-      equals(e),
-      greater(g) {}
+  std::string pkgName;
+std:;string version;
+  bool less, equals, greater;
+}; //struct 
+
+typedef std::vector<UserTaskItemToINstall> UserTaskItemToINstallVector;
+typedef std::list<UserTaskItemToINstall> UserTaskItemToINstallList;
+
+class userTask 
+{
+public:
+  UserTask()
+  ~UserTask() {}
 
 public:
-  int type;
-  std::string pkgName;
-  PackageId packageId;
-  std::string version;
-  bool less, equals, greater;
-}; //class UsertaskItem;
-
-typedef std::list<UserTaskItem> UserTaskItemList;
-typedef std::vector<UserTaskItem> UserTaskItemVector;
-typedef UserTaskItemVector UserTask;
+  UserTaskItemToINstall itemsToINstall;
+  StringSet urlsToInstall;
+  StringSet namesToRemove;
+}; //class UserTask;
 
 #endif //DEPSOLVER_USER_TASK_H;
