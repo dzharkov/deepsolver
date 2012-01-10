48b69f78507b3050bea3708a8d51e5b8cbe9692d
Michael Pozhidaev <msp@altlinux.ru>
2012-01-10 13:39:29 +0700

    ChangesLog.h content and NamedPkgRel.h moved to Pkg.h

--- lib/Pkg.h
+++ lib/Pkg.h
@@ -2,10 +2,69 @@
 #ifndef DEPSOLVER_PKG_H
 #define DEPSOLVER_PKG_H
 
-#include"NamedPkgRel.h"
-#include"ChangeLog.h"
-
 typedef unsigned short Epoch;
+
+/**\brief The relation between two packages with package specifications by name
+ *
+ * This class contains information about one relaytion between two
+ * packages. Relation data includes version specification and relation
+ * type with values from the list "less", "less or equals", "equals",
+ * "greater or equals", "greater". One package from the relation defined
+ * explicitly by its name in this class, the second one defined
+ * implicitly by the owner of this class instance. Package specification
+ * by its name is not the single way, since it can take a lot of memory
+ * to store package name strings. In some cases the better way to use
+ * package identifiers instead of real name strings. The usual purpose of
+ * this class is to save data about package provides, requires, conflicts
+ * or obsoletes entries.
+ */
+class NamedPkgRel
+{
+public:
+  enum {
+    Less = 1,
+    Equals = 2,
+    Greater = 4
+  };
+
+  NamedPkgRel()
+    : type(0) {}
+
+  NamedPkgRel(const std::string& pName)
+    : pkgName(pName), type(0) {}
+
+  NamedPkgRel(const std::string& pName, char t, const std::string& v)
+    : pkgName(pName), type(t), ver(v) {}
+
+public:
+  std::string pkgName;
+  char type;
+  std::string ver;
+}; //class NamedPkgRel;
+
+std::ostream& operator <<(std::ostream& s, const NamedPkgRel& r);
+
+typedef std::list<NamedPkgRel> NamedPkgRelList;
+typedef std::vector<NamedPkgRel> NamedPkgRelVector;
+
+class ChangeLogEntry
+{
+public:
+  ChangeLogEntry()
+    : time(0) {}
+
+  ChangeLogEntry(time_t tm, const std::string& nm, const std::string& txt)
+    : time(tm), name(nm), text(txt) {}
+
+public:
+  time_t time;
+  std::string name;
+  std::string text;
+}; //class ChangeLogEntry;
+
+typedef std::vector<ChangeLogEntry> ChangeLogEntryVector;
+typedef std::list<ChangeLogEntry> ChangeLogEntryList;
+typedef ChangeLogEntryVector ChangeLog;
 
 class PkgBase
 {
