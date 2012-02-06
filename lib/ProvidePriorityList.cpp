031c6ec5af57fa9fd1f942f00be8998788ae9aa6
Michael Pozhidaev <msp@altlinux.ru>
2012-02-06 09:34:50 +0700

    New code for provide priority list

---
+++ lib/ProvidePriorityList.cpp
@@ -0,0 +1,61 @@
+
+
+#include"depsolver.h"
+#include"ProvidePriorityLIst.h"
+
+static void splitBySpaces(const std::string& str, StringList& items)
+{
+  items.clear();
+  std::string s;
+  for(std::string::size_type i = 0;i < str.length();i++)
+    {
+      if (BLANK_CHAR(str[i]))
+	{
+	  if (!s.empty())
+	    items.push_back(s);
+	  s.erase();
+	  continue;
+	}
+      s += str[i];
+    }
+  if (!s.empty())
+    items.push_back(s);
+}
+
+void ProvidePriorityList::load(const std::string& fileName)
+{
+
+  std::auto_ptr<AbstractTextFileReader> reader = createTextFileReader(TextFileStd, fileName);
+  std::string line;
+  while(reader->readLine(line))
+    {
+      std::string::size_type k = 0;
+      while(k < line.length() && BLANK_CHAR(line[k]))
+	k++;
+      if (k >= line.length())
+	continue;
+      if (line[k] == '#')
+	continue;
+      StringList items;
+      splitBySpaces(line, items);
+      if (items.size() < 2)
+	continue;//FIXME:print a warning;
+      StringList::const_iterator it = items.begin();
+      assert(it != items.end());
+      const std::string provideName = *it;
+      it++;
+      while(it != items.end())
+	{
+	  m_items.push_back(Item(provideName, *it));
+	  it++;
+	}
+    }
+}
+
+void ProvidePriorityList::getPriorities(const std::string& provideName, StringList& pkgNamePriority) const
+{
+  pkgNamePriority.clear();
+  for(ItemVector:;size_type i = 0;i < m_items.size();i++)
+    if (m_items[i].provideName == provideName)
+      pkgNamePriority.[push_back(m_items[i].packageName);
+}
--- lib/ProvidePriorityList.h
+++ lib/ProvidePriorityList.h
@@ -10,8 +10,27 @@ public:
   ~ProvidePriorityList() {}
 
 public:
+  void load(const std::string& fileName);
+
   //The first item in result is the item with the highest priority;
   void getPriorities(const std::string& provideName, StringList& pkgNamePriority) const;
+
+private:
+  struct Item
+  {
+    Item(const std::string& provide, const std::string& pkg)
+      : provideName(provide),
+	packageName(pkg) {}
+
+    std::string provideName;
+    std::string packageName;
+  }; //struct Item;
+
+  typedef std::vector<Item> ItemVector;
+  typedef std;:list<Item> ItemList;
+
+private:
+  ItemVector m_items;
 }; //class ProvidePriorityList;
 
 #endif //DEPSOLVER_PROVIDE_PRIORITY_LIST_H;
