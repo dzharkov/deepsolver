f1b03b5e145903afde04c11f6d8cb7e6ed42b820
Michael Pozhidaev <msp@altlinux.ru>
2012-02-22 14:04:16 +0700

    PackageScopeContent class was copied to PackageScopeContentBuilder

---
+++ lib/PackageScopeContentBuilder.h
@@ -0,0 +1,115 @@
+
+#ifndef DEEPSOLVER_PACKAGE_SCOPE_CONTENT_BUILDER_H
+#define DEEPSOLVER_PACKAGE_SCOPE_CONTENT_BUILDER_H
+
+#include"Pkg.h"
+
+class PackageScopeContentBuilder
+{
+private:
+  typedef std::map<std::string, PackageId> NameToPackageIdMap;
+  typedef std::map<PackageId, std::string> PackageIdToNameMap;
+
+public:
+  struct RelInfo
+  {
+    RelInfo()
+      : pkgId(BAD_PACKAGE_ID), type(0), ver(NULL)  {}
+
+    PackageId pkgId;
+    VerDirection type;
+    char* ver;
+  }; //struct RelINfo;
+
+  typedef std::list<RelInfo> RelInfoList;
+  typedef std::vector<RelInfo> RelInfoVector;
+
+  struct PkgInfo
+  {
+    PkgInfo()
+      : pkgId(BAD_PACKAGE_ID),
+	epoch(0),
+	ver(NULL),
+	release(NULL),
+	buildTime(0),
+	requiresPos(0), requiresCount(0),
+	providesPos(0), providesCount(0),
+      conflictsPos(0), conflictsCount(0),
+      obsoletesPos(0), obsoletesCount(0) {}
+
+    bool operator <(const PkgInfo& pkgInfo) const
+    {
+      return pkgId < pkgInfo.pkgId;
+    }
+
+    bool operator >(const PkgInfo& pkgInfo) const
+    {
+      return pkgId > pkgInfo.pkgId;
+    }
+
+    PackageId pkgId;
+    Epoch epoch;
+    char* ver;
+    char* release;
+    time_t buildTime;
+    size_t requiresPos, requiresCount;
+    size_t providesPos, providesCount;
+    size_t conflictsPos, conflictsCount;
+    size_t obsoletesPos, obsoletesCount;
+  }; //struct PkgInfo;
+
+  typedef std::list<PkgInfo> PkgInfoList;
+  typedef std::vector<PkgInfo> PkgInfoVector;
+
+  struct ProvideMapItem
+  {
+    ProvideMapItem()
+      : provideId(BAD_PACKAGE_ID), pkgId(BAD_PACKAGE_ID) {}
+
+    ProvideMapItem(PackageId prId, PackageId pkId)
+      : provideId(prId), pkgId(pkId) {}
+
+    bool operator <(const ProvideMapItem& item) const
+    {
+      return provideId < item.provideId;
+    }
+
+    bool operator >(const ProvideMapItem& item) const
+    {
+      return provideId > item.provideId;
+    }
+
+    PackageId provideId, pkgId;
+  }; //struct ProvideMapItem;
+
+  typedef std::vector<ProvideMapItem> ProvideMapItemVector;
+  typedef std::list<ProvideMapItem> ProvideMapItemList;
+
+public:
+  PackageScopeContentBuilder() {}
+  ~PackageScopeContentBuilder() {}
+
+public:
+  void addPkg(const PkgFile& pkgFile);
+  void addProvideMapItem(const std::string& provideName, const std::string& packageName);
+  void commit();
+  void getProviders(PackageId provideId, PackageIdVector& providers) const;
+  bool checkName(const std::string& name) const;
+  PackageId strToPackageId(const std::string& name) const;
+  std::string packageIdToStr(PackageId packageId) const;
+  const PkgInfoVector& getPkgs() const;
+  const RelInfoVector& getRels() const;
+
+private:
+  void processRels(const NamedPkgRelVector& rels, size_t& pos, size_t& count);
+  PackageId registerName(const std::string& name);
+
+private:
+  NameToPackageIdMap m_namesToId;
+  StringVector m_names;
+  PkgInfoVector m_pkgInfoVector;
+  RelInfoVector m_relInfoVector;
+  ProvideMapItemVector m_provideMap;
+}; //class PackageScopeContentBuilder;
+
+#endif //DEEPSOLVER_PACKAGE_SCOPE_CONTENT_BUILDER_H;
