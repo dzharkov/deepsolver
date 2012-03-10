74e4f44819ad81586147698d6a0ec0a573863811
Michael Pozhidaev <msp@altlinux.ru>
2012-03-11 03:31:30 +0700

    Copyright info update

--- lib/DeepsolverException.h
+++ lib/DeepsolverException.h
@@ -1,3 +1,20 @@
+/*
+   Copyright 2011-2012 ALT Linux
+   Copyright 2011-2012 Dmitry V. Levin
+   Copyright 2011-2012 Michael Pozhidaev
+
+   This file is part of the Deepsolver.
+
+   Deepsolver is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   Deepsolver is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 #ifndef DEEPSOLVER_DEEPSOLVER_EXCEPTION_H
 #define DEEPSOLVER_DEEPSOLVER_EXCEPTION_H
