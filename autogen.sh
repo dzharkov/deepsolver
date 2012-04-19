6f55d3ecd92eda99691b0a238b236e1f7da21a6c
Michael Pozhidaev <msp@altlinux.ru>
2012-04-19 07:49:42 +0700

    Added autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+#created by aclocal;
+/bin/rm -rf autom4te.cache
+/bin/rm -f aclocal.m4
+
+#created by libtoolize;
+/bin/rm -rf m4
+/bin/rm -f ltmain.sh
+
+#created by autoconf;
+/bin/rm -f configure
+
+#created by automake;
+/bin/rm -f install-sh missing depcomp Makefile.in config.guess config.sub
+
+[ "$1" == 'clean' ] && exit 0
+
+IPATHS="-I lib"
+aclocal $IPATHS
+libtoolize
+autoconf $IPATHS
+automake --add-missing
+
+/bin/rm -rf autom4te.cache
+echo Now run ./configure and then make.
