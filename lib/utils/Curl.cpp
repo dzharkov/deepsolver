f782eef4fa9b85dfbea8dacf1e98e1fb1a78a9a5
Michael Pozhidaev <msp@altlinux.ru>
2012-04-24 12:52:26 +0700

    Curl interface beta

--- lib/utils/Curl.cpp
+++ lib/utils/Curl.cpp
@@ -17,9 +17,12 @@ static int curlProgress(void* p,
 			double ulTotal,
 			double ulNow)
 {
+  AbstractCurlProgressListener* progressListener = (AbstractCurlProgressListener*)p;
+  assert(progressListener != NULL);
   const size_t now = (size_t)dlNow;
   const size_t total = (size_t)dlTotal;
-  std::cout << now << "/" << total << std::endl;
+  if (total > 0)
+    progressListener->onCurlProgress(now, total);
   return 0;
 }
 
@@ -59,7 +62,9 @@ void CurlInterface::close()
   m_handle = NULL;
 }
 
-void CurlInterface::fetch(const std::string& url, AbstractCurlDataRecipient& recipient)
+void CurlInterface::fetch(const std::string& url,
+			  AbstractCurlDataRecipient& recipient,
+			  AbstractCurlProgressListener& progressListener)
 {
   assert(!url.empty());
   CURL* handle = (CURL*)m_handle;
@@ -68,54 +73,12 @@ void CurlInterface::fetch(const std::string& url, AbstractCurlDataRecipient& rec
   //Uncomment the following line if you want to see debug messages from libcurl on your console;
   //curl_easy_setopt(handle, CURLOPT_VERBOSE, 1L);
   curl_easy_setopt(handle, CURLOPT_NOPROGRESS, 0L);
-
   curl_easy_setopt(handle, CURLOPT_PROGRESSFUNCTION, curlProgress);
-  curl_easy_setopt(handle, CURLOPT_PROGRESSDATA, &recipient);//FIXME:
-
-
+  curl_easy_setopt(handle, CURLOPT_PROGRESSDATA, &progressListener);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, acceptCurlData);
   curl_easy_setopt(handle, CURLOPT_FILE, &recipient);
   const CURLcode res = curl_easy_perform(handle);
   if (res)
-    throw CurlException(res, curl_easy_strerror(res));
+    throw CurlException(res, url, curl_easy_strerror(res));
 }
 
-class DataRecipient: public AbstractCurlDataRecipient
-{
-public:
-  DataRecipient(int fd)
-    : m_fd(fd) {}
-
-  virtual ~DataRecipient() {}
-
-public:
-  size_t onNewDataBlock(const void* buf, size_t bufSize)
-  {
-    write(m_fd, buf, bufSize);
-    return bufSize;
-  }
-
-private:
-  int m_fd;
-};
-
-int main()
-{
-  int fd = open("/tmp/proba.bin", O_WRONLY | O_CREAT);
-  assert(fd >= 0);
-  DataRecipient recipient(fd);
-  curlInitialize();
-  CurlInterface curl;
-  curl.init();
-  try {
-    //    curl.fetch("http://deepsolver.altlinux.org/tech-assignment.iso", recipient);
-
-  }
-  catch(const CurlException& e)
-    {
-      std::cerr << "curl:" << e.getMessage() << std::endl;
-    }
-  curl.close();
-  close(fd);
-  return 0;
-}
--- lib/utils/Curl.h
+++ lib/utils/Curl.h
@@ -6,8 +6,8 @@
 class CurlException //FIXME: : public DeepsolverException
 {
 public:
-  CurlException(int code, const std::string& message)
-    : m_code(code), m_message(message) {}
+  CurlException(int code, const std::string& url, const std::string& text)
+    : m_code(code), m_url(url), m_text(text) {}
 
   virtual ~CurlException() {}
 
@@ -17,6 +17,16 @@ public:
     return m_code;
   }
 
+  std::string getUrl() const
+  {
+    return m_url;
+  }
+
+  std::string getText() const
+  {
+    return m_text;
+  }
+
   std::string getType() const
   {
     return "curl";
@@ -24,12 +34,13 @@ public:
 
   std::string getMessage() const
   {
-    return m_message;
+    return m_url + ":" + m_text;
   }
 
 private:
   const int m_code;
-  const std::string m_message;
+  const std::string m_url;
+  const std::string m_text;
 }; //class CurlException;
 
 class AbstractCurlDataRecipient
@@ -39,6 +50,15 @@ public:
 
 public:
   virtual size_t onNewDataBlock(const void* buf, size_t bufSize) = 0;
+}; //class AbstractCurlDataRecipient;
+
+class AbstractCurlProgressListener
+{
+public:
+  virtual ~AbstractCurlProgressListener() {}
+
+public:
+  virtual void onCurlProgress(size_t now, size_t total) = 0;
 }; //class AbstractCurlDataRecipient;
 
 class CurlInterface
@@ -55,7 +75,10 @@ public:
 public:
   void init();
   void close();
-  void fetch(const std::string& url, AbstractCurlDataRecipient& recipient);
+
+  void fetch(const std::string& url,
+	     AbstractCurlDataRecipient& recipient,
+	     AbstractCurlProgressListener& progressListener);
 
 private:
   void* m_handle;
