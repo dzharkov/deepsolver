1aa0b25b5e9a7211f2ce82ad7044c3ec86d27634
Michael Pozhidaev <msp@altlinux.ru>
2012-05-27 02:25:32 +0700

    Initial InfoCore with function to retrieve list of available packages

---
+++ lib/InfoCore.cpp
@@ -0,0 +1,35 @@
+/*
+   Copyright 2011-2012 ALT Linux
+   Copyright 2011-2012 Michael Pozhidaev
+
+   This file is part of the Deepsolver.
+
+   Deepsolver is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   Deepsolver is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+#include"deepsolver.h"
+#include"InfoCore.h"
+#include"PackageScopeContentLoader.h"
+
+void InfoCore::availablePackages(PkgVector& pkgs)
+{
+  PackageScopeContentLoader content;
+  content.loadFromFile(Directory::mixNameComponents(m_conf.root().dir.pkgData, PKG_DATA_FILE_NAME));
+  const PackageScopeContent::PkgInfoVector& p = content.getPkgs();
+  pkgs.resize(p.size());
+  for(PackageScopeContent::PkgInfoVector::size_type i = 0;i < p.size();i++)
+    {
+      pkgs[i].name = content.packageIdToStr(p[i].pkgId);
+      pkgs[i].epoch = p[i].epoch;
+      pkgs[i].version = p[i].ver;
+      pkgs[i].release = p[i].release;
+    }
+}
---
+++ lib/InfoCore.h
@@ -0,0 +1,39 @@
+/*
+   Copyright 2011-2012 ALT Linux
+   Copyright 2011-2012 Michael Pozhidaev
+
+   This file is part of the Deepsolver.
+
+   Deepsolver is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   Deepsolver is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+#ifndef DEEPSOLVER_INFO_CORE_H
+#define DEEPSOLVER_INFO_CORE_H
+
+#include"ConfigCenter.h"
+#include"Pkg.h"
+
+class InfoCore
+{
+public:
+  InfoCore(const ConfigCenter& conf): 
+    m_conf(conf)  {}
+
+    virtual ~InfoCore() {}
+
+public:
+    void availablePackages(PkgVector& pkgs);
+
+private:
+  const ConfigCenter& m_conf;
+}; //class InfoCore;
+
+#endif //DEEPSOLVER_INFO_CORE_H;
