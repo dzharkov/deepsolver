deb6a2acde8b9f91cf06383ea20607b0636878a8
Michael Pozhidaev <msp@altlinux.ru>
2012-07-09 04:12:59 +0700

    Documentation for InfoFileWriter

--- lib/repo/InfoFileWriter.h
+++ lib/repo/InfoFileWriter.h
@@ -18,13 +18,36 @@
 #ifndef DEEPSOLVER_INFO_FILE_WRITER_H
 #define DEEPSOLVER_INFO_FILE_WRITER_H
 
+/**\brief Constructs and writes repository information file
+ *
+ * This class takes a string to string map and saves it as a file using
+ * the repository information file format. New file beginning contains
+ * additional comment with short purpose and format description.
+ *
+ * The values of provided string to string map can be strings with any
+ * content keeping in mind all blank characters in value beginning and
+ * ending will be silently skipped on reading. The map keys can contain
+ * only small and capital latin letters, digits, dashes and underlines.
+ *
+ * \sa IndexCore InfoFileReader TextFormatWriter
+ */
 class InfoFileWriter
 {
 public:
+  /**\brief The default constructor*/
   InfoFileWriter() {}
+
+  /**\brief The destructor*/
   virtual ~InfoFileWriter() {}
 
 public:
+  /**\brief Saves provided map to disk
+   *
+   * This method throws SystemException on any I/O problems.
+   *
+   * \param [in] fileName The name of the file to save to
+   * \param [in] params The user defined string to string map to save
+   */
   void write(const std::string& fileName, const StringToStringMap& params) const;
 }; //class InfoFileWriter;
 
