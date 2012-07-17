afe8c984b420bc8b23ba84f44291c2b83422f4fd
Michael Pozhidaev <msp@altlinux.ru>
2012-07-17 16:09:54 +0700

    Intermediate commit

--- lib/repo/TextFormatWriter.cpp
+++ lib/repo/TextFormatWriter.cpp
@@ -19,7 +19,7 @@
 #include"TextFormatWriter.h"
 #include"IndexCoreException.h"
 
-#define TMP_FILE "tmp_packages_data1"
+#define TMP_FILE "tmp_packages_data"
 
 #define NAME_STR "n="
 #define EPOCH_STR "e="
@@ -49,9 +49,13 @@ TextFormatWriter::TextFormatWriter(AbstractIndexConstructionListener& listener,
     m_requireFilter(requireFilter),
     m_params(params),
     m_dir(dir),
-    m_packagesFileName(addCompressionExtension(Directory::mixNameComponents(dir, REPO_INDEX_PACKAGES_DATA_FILE), params)),
-    m_sourcesFileName(addCompressionExtension(Directory::mixNameComponents(dir, REPO_INDEX_SOURCE_DATA_FILE), params)),
+
     m_tmpFileName(Directory::mixNameComponents(dir, TMP_FILE)),
+    m_packagesFileName(addCompressionExtension(Directory::mixNameComponents(dir, REPO_INDEX_PACKAGES_FILE), params)),
+    m_packagesDescrFileName(addCompressionExtension(Directory::mixNameComponents(dir, REPO_INDEX_PACKAGES_DESCR_FILE), params)),
+    m_sourcesFileName(addCompressionExtension(Directory::mixNameComponents(dir, REPO_INDEX_SOURCE_FILE), params)),
+
+    m_sourcesDescrFileName(addCompressionExtension(Directory::mixNameComponents(dir, REPO_INDEX_SOURCES_DESCR_FILE), params)),
     m_filterProvidesByRefs(params.provideFilteringByRefs),
     m_additionalRefs(additionalRefs),
     m_filterProvidesByDirs(params.provideFilterDirs)
@@ -60,33 +64,38 @@ TextFormatWriter::TextFormatWriter(AbstractIndexConstructionListener& listener,
 
 void TextFormatWriter::initBinary()
 {
-  m_tmpFile = createTextFileWriter(TextFileStd, m_tmpFileName);
+  if (m_filterProvidesByRefs)//It means the second phase is needed;
+    m_packagesFile = createTextFileWriter(TextFileStd, m_tmpFileName); else
+    m_packagesFile = createTextFileWriter(mapTextFileType(m_params.compressionType), m_packagesFileName);
+    m_packagesDescrFile = createTextFileWriter(mapTextFileType(m_params.compressionType), m_packagesDescrFileName);
 }
 
 void TextFormatWriter::initSource()
 {
   m_sourcesFile = createTextFileWriter(mapTextFileType(m_params.compressionType), m_sourcesFileName);
+  m_sourcesDescrFile = createTextFileWriter(mapTextFileType(m_params.compressionType), m_sourcesDescrFileName);
 }
 
 void TextFormatWriter::addBinary(const PkgFile& pkgFile, const StringList& fileList)
 {
-  m_tmpFile->writeLine("[" + File::baseName(pkgFile.fileName) + "]");
-  m_tmpFile->writeLine(NAME_STR + pkgFile.name);
+  m_packagesFile->writeLine("[" + File::baseName(pkgFile.fileName) + "]");
+  m_packagesDescrFile->writeLine("[" + File::baseName(pkgFile.fileName) + "]");
+  m_packagesFile->writeLine(NAME_STR + pkgFile.name);
   std::ostringstream epochStr;
   epochStr << EPOCH_STR << pkgFile.epoch;
-  m_tmpFile->writeLine(epochStr.str());
-  m_tmpFile->writeLine(VERSION_STR + pkgFile.version);
-  m_tmpFile->writeLine(RELEASE_STR + pkgFile.release);
-  m_tmpFile->writeLine(ARCH_STR + pkgFile.arch);
-  m_tmpFile->writeLine(URL_STR + pkgFile.url);
-  m_tmpFile->writeLine(LICENSE_STR + pkgFile.license);
-  m_tmpFile->writeLine(PACKAGER_STR + pkgFile.packager);
-  m_tmpFile->writeLine(SUMMARY_STr + pkgFile.summary);
-  m_tmpFile->writeLine(DESCRIPTION_STR + encodeMultiline(pkgFile.description));
-  m_tmpFile->writeLine(SRCRPM_STR + pkgFile.srcRpm);
+  m_packagesFile->writeLine(epochStr.str());
+  m_packagesFile->writeLine(VERSION_STR + pkgFile.version);
+  m_packagesFile->writeLine(RELEASE_STR + pkgFile.release);
+  m_packagesFile->writeLine(ARCH_STR + pkgFile.arch);
+  m_packagesDescrFile->writeLine(URL_STR + pkgFile.url);
+  m_packagesDescrFile->writeLine(LICENSE_STR + pkgFile.license);
+  m_packagesDescrFile->writeLine(PACKAGER_STR + pkgFile.packager);
+  m_packagesDescrFile->writeLine(SUMMARY_STr + pkgFile.summary);
+  m_packagesDescrFile->writeLine(DESCRIPTION_STR + encodeMultiline(pkgFile.description));
+  m_packagesFile->writeLine(SRCRPM_STR + pkgFile.srcRpm);
   std::ostringstream bt;
   bt << pkgFile.buildTime;
-  m_tmpFile->writeLine(BUILDTIME_STR + bt.str());
+  m_packagesFile->writeLine(BUILDTIME_STR + bt.str());
   for(NamedPkgRelVector::const_iterator it = pkgFile.provides.begin();it != pkgFile.provides.end();it++)
     {
       /*
@@ -95,7 +104,7 @@ void TextFormatWriter::addBinary(const PkgFile& pkgFile, const StringList& fileL
        * saving all provides, if filtering is enabled we will proceed real
        * filtering on additional phase.
        */
-      m_tmpFile->writeLine(PROVIDES_STR + saveNamedPkgRel(*it));
+      m_packagesFile->writeLine(PROVIDES_STR + saveNamedPkgRel(*it));
     }
   for(StringList::const_iterator it = fileList.begin();it != fileList.end();it++)
     /*
@@ -107,67 +116,66 @@ void TextFormatWriter::addBinary(const PkgFile& pkgFile, const StringList& fileL
      * directory presents in directory list.
      */
     if (m_filterProvidesByRefs || m_filterProvidesByDirs.empty() || fileFromDirs(*it, m_filterProvidesByDirs))
-      {
-	m_tmpFile->writeLine(PROVIDES_STR + saveFileName(*it));
-      }
+      m_packagesFile->writeLine(PROVIDES_STR + saveFileName(*it));
   for(NamedPkgRelVector::const_iterator it = pkgFile.requires.begin();it != pkgFile.requires.end();it++)
     {
       if (m_requireFilter.excludeRequire(it->pkgName))
 	continue;
-      m_tmpFile->writeLine(REQUIRES_STR + saveNamedPkgRel(*it));
+      m_packagesFile->writeLine(REQUIRES_STR + saveNamedPkgRel(*it));
       if (m_filterProvidesByRefs)
 	m_refsSet.insert(it->pkgName);
     }
   for(NamedPkgRelVector::const_iterator it = pkgFile.conflicts.begin();it != pkgFile.conflicts.end();it++)
     {
-      m_tmpFile->writeLine(CONFLICTS_STR + saveNamedPkgRel(*it));
+      m_packagesFile->writeLine(CONFLICTS_STR + saveNamedPkgRel(*it));
       if (m_filterProvidesByRefs)
 	m_refsSet.insert(it->pkgName);
     }
   for(NamedPkgRelVector::const_iterator it = pkgFile.obsoletes.begin();it != pkgFile.obsoletes.end();it++)
-    m_tmpFile->writeLine(OBSOLETES_STR + saveNamedPkgRel(*it));
+    m_packagesFile->writeLine(OBSOLETES_STR + saveNamedPkgRel(*it));
   if (m_params.changeLogBinary)
     for(ChangeLog::size_type i = 0;i < pkgFile.changeLog.size();i++)
-      m_tmpFile->writeLine(CHANGELOG_STR + encodeChangeLogEntry(pkgFile.changeLog[i]));
-  m_tmpFile->writeLine("");
+      m_packagesDescrFile->writeLine(CHANGELOG_STR + encodeChangeLogEntry(pkgFile.changeLog[i]));
+  m_packagesFile->writeLine("");
+  m_packagesDescrFile->writeLine("");
 }
 
 void TextFormatWriter::addSource(const PkgFile& pkgFile)
 {
   m_sourcesFile->writeLine("[" + File::baseName(pkgFile.fileName) + "]");
+  m_sourcesDescrFile->writeLine("[" + File::baseName(pkgFile.fileName) + "]");
   m_sourcesFile->writeLine(NAME_STR + pkgFile.name);
   std::ostringstream epochStr;
   epochStr << EPOCH_STR << pkgFile.epoch;
   m_sourcesFile->writeLine(epochStr.str());
   m_sourcesFile->writeLine(VERSION_STR + pkgFile.version);
   m_sourcesFile->writeLine(RELEASE_STR + pkgFile.release);
-  //No nned need to write arch for source packages;
-  m_sourcesFile->writeLine(URL_STR + pkgFile.url);
-  m_sourcesFile->writeLine(LICENSE_STR + pkgFile.license);
-  m_sourcesFile->writeLine(PACKAGER_STR + pkgFile.packager);
-  m_sourcesFile->writeLine(SUMMARY_STr + pkgFile.summary);
-  m_sourcesFile->writeLine(DESCRIPTION_STR + encodeMultiline(pkgFile.description));
+  //No need need to write arch for source packages;
+  m_sourcesDescrFile->writeLine(URL_STR + pkgFile.url);
+  m_sourcesDescrFile->writeLine(LICENSE_STR + pkgFile.license);
+  m_sourcesDescrFile->writeLine(PACKAGER_STR + pkgFile.packager);
+  m_sourcesDescrFile->writeLine(SUMMARY_STr + pkgFile.summary);
+  m_sourcesDescrFile->writeLine(DESCRIPTION_STR + encodeMultiline(pkgFile.description));
   //No need to write src.rpm entry, usually it is empty for source packages;
   if (m_params.changeLogSources)
     for(ChangeLog::size_type i = 0;i < pkgFile.changeLog.size();i++)
-      m_sourcesFile->writeLine(CHANGELOG_STR + encodeChangeLogEntry(pkgFile.changeLog[i]));
+      m_sourcesDescrFile->writeLine(CHANGELOG_STR + encodeChangeLogEntry(pkgFile.changeLog[i]));
   m_sourcesFile->writeLine("");
+  m_sourcesDescrFile->writeLine("");
 }
 
 void TextFormatWriter::commitBinary()
 {
-  m_tmpFile->close();
+  m_packagesFile->close();
+  m_packagesDescrFile->close();
   if (m_filterProvidesByRefs)
-    {
-      secondPhase();
-    }
-  logMsg(LOG_DEBUG, "Removing \'%s\'", m_tmpFileName.c_str());
-  File::unlink(m_tmpFileName);
+    secondPhase();
 }
 
 void TextFormatWriter::commitSource()
 {
   m_sourcesFile->close();
+  m_sourcesDescrFile->close();
 }
 
 void TextFormatWriter::secondPhase()
@@ -175,7 +183,7 @@ void TextFormatWriter::secondPhase()
   assert(m_filterProvidesByRefs);
   const std::string inputFileName = m_tmpFileName, outputFileName = m_packagesFileName;
   std::auto_ptr<AbstractTextFileReader> inputFile = createTextFileReader(TextFileStd, inputFileName);
-  std::auto_ptr<AbstractTextFileWriter> outputFile = createTextFileWriter(TextFileStd, outputFileName);
+  std::auto_ptr<AbstractTextFileWriter> outputFile = createTextFileWriter(TextFileStd, outputFileName);//FIXME:map;
   std::string name, line;
   while (inputFile->readLine(line))
     {
