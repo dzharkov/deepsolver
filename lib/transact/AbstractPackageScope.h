75dfc37f81f95339a6cc52da00ce7b4e617beb00
Michael Pozhidaev <msp@altlinux.ru>
2012-11-19 10:57:40 +0700

    AbstractPackageScope.h added

---
+++ lib/transact/AbstractPackageScope.h
@@ -0,0 +1,48 @@
+/*
+   Copyright 2011-2012 ALT Linux
+   Copyright 2011-2012 Michael Pozhidaev
+
+   This file is part of the Deepsolver.
+
+   Deepsolver is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   Deepsolver is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+#ifndef DEEPSOLVER_ABSTRACT_PACKAGE_SCOPE_H
+#define ABSTRACT_PACKAGE_SCOPE_H
+
+class AbstractPackageScope
+{
+public:
+  AbstractPackageScope() {}
+  virtual ~AbstractPackageScope() {}
+
+public:
+  bool allProvidesHaveTheVersion(const VarIdVector& vars, PackageId provideEntry);
+  bool checkName(const std::string& name);
+  std::string constructPackageNameWithBuildTime(VarId varId);
+  std::string constructPackageNameWithBuildTime(VarId varId);
+  void getConflicts(VarId varId, IdPkgRelVector& res);
+  void getRequires(VarId varId, IdPkgRelVector& res);
+  bool isInstalledWithMatchingAlternatives(VarId varId);
+  PackageId packageIdOfVarId(VarId varId);
+  std::string packageIdToStr(PackageId packageId);
+  void selectMatchingVarsAmongProvides(PackageId packageId, const VersionCond& ver, VarIdVector& vars);
+  void selectMatchingVarsRealNames(PackageId packageId, const VersionCond& ver, VarIdVector& vars);
+  void selectMatchingVarsWithProvides(PackageId packageId, const VersionCond& ver, VarIdVector& vars);
+  void selectTheNewest(VarIdVector& vars);
+  void selectTheNewestByProvide(VarIdVector& vars, PackageId provideEntry);
+  PackageId strToPackageId(const std::string& name);
+  void whatConflictsAmongInstalled(VarId varId, VarIdVector& res, IdPkgRelVector& resRels);
+  void whatDependsAmongInstalled(VarId varId, VarIdVector& res, IdPkgRelVector& resRels);
+  void whatSatisfiesAmongInstalled(const IdPkgRel& rel, VarIdVector& res);
+}; //class AbstractPackageScope;
+
+#endif //DEEPSOLVER_ABSTRACT_PACKAGE_SCOPE_H;
