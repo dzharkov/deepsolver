1cb56aa428f7c1e2a3f24f918fb09491fd446003
Michael Pozhidaev <msp@altlinux.ru>
2012-12-25 01:03:15 +0700

    PkgSorting.h added

--- lib/PkgSorting.h
+++ lib/PkgSorting.h
@@ -1,28 +1,53 @@
+/*
+   Copyright 2011-2012 ALT Linux
+   Copyright 2011-2012 Michael Pozhidaev
 
-struct PrioritySortItem
+   This file is part of the Deepsolver.
+
+   Deepsolver is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   Deepsolver is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+#ifndef DEEPSOLVER_PACKAGE_NAME_SORTING_H
+#define DEEPSOLVER_PACKAGE_NAME_SORTING_H
+
+#include"AbstractPackageBackEnd.h"
+
+struct PackageNameSortItem
 {
-  PrioritySortItem(const AbstractPackageBackEnd& b,
+  PackageNameSortItem(const AbstractPackageBackEnd* b,
 		   VarId v,
 		   const std::string& n)
     : backEnd(b),
       varId(v),
-      name(n) {}
-
-  bool operator <(const PrioritySortItem& item) const
+      name(n) 
   {
-    return backEnd.versionCompare(name, item.name) < 0;
+    assert(backEnd != NULL);
   }
 
-  bool operator >(const PrioritySortItem& item) const
+  bool operator <(const PackageNameSortItem& item) const
   {
-    return backEnd.versionCompare(name, item.name) > 0;
+    return backEnd->versionCompare(name, item.name) < 0;
   }
 
-  const AbstractPackageBackEnd& backEnd;
+  bool operator >(const PackageNameSortItem& item) const
+  {
+    return backEnd->versionCompare(name, item.name) > 0;
+  }
+
+  const AbstractPackageBackEnd* backEnd;
   VarId varId;
   std::string name;
 }; //struct PrioritySortItem;
 
-typedef std::vector<PrioritySortItem> PrioritySortItemVector;
-typedef std::list<PrioritySortItem> PrioritySortItemList;
+typedef std::vector<PackageNameSortItem> PackageNameSortItemVector;
+typedef std::list<PackageNameSortItem> PackageNameSortItemList;
 
+#endif //DEEPSOLVER_PACKAGE_NAME_SORTING_H;
