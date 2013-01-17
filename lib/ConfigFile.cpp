ffd0de5fb8df458ee142571f22baa3c5cd321c9c
Michael Pozhidaev <msp@altlinux.ru>
2013-01-17 11:10:19 +0700

    ConfigFile:fixed bug with "=" sign just after name without spaces

--- lib/ConfigFile.cpp
+++ lib/ConfigFile.cpp
@@ -228,6 +228,7 @@ void ConfigFile::processValue(const std::string& line)
 	  stopParam(state, i, line);
 	  assert(0);
 	} //PARAM_INITIAL;
+      //PARAM_NAME;
       if (state == PARAM_NAME)
 	{
 	  if (validIdentChar(c))
@@ -240,6 +241,30 @@ void ConfigFile::processValue(const std::string& line)
 	      m_path.push_back(name);
 	      name.erase();
 	      state = PARAM_AFTER_NAME;
+	      continue;
+	    }
+	  if (c == '=')
+	    {
+	      m_path.push_back(name);
+	      name.erase();
+	      state = PARAM_VALUE;
+	      m_assignMode = ModeAssign;
+	      continue;
+	    }
+	  if (c == '+')
+	    {
+	      if (i + 1 >= line.length())
+		break;
+	      i++;
+	      if (line[i] != '=')
+		{
+		  stopParam(state, i, line);
+		  assert(0);
+		}
+	      m_path.push_back(name);
+	      name.erase();
+	      state = PARAM_VALUE;
+	      m_assignMode = ModeAdding;
 	      continue;
 	    }
 	  if (c == '.')
